81e65902040865cba4785f6388ad6ae966ec4c24
JV <preubecitas@gmail.com>
2024-05-09 15:28:48 -0600

    [🚧backup] Avances 3 para la impresión de PDFS

--- resources/views/exports/pdf.blade.php
+++ resources/views/exports/pdf.blade.php
@@ -7,93 +7,84 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ __('Receipt') }}</title>
 
-    <link rel="stylesheet" type="text/css" href="{{ public_path('assets/css/pdf.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ public_path('assets/css/recepit.css') }}">
 </head>
 
 <body>
-    <div class="invoice">
+    <div class="rounded">
 
         <h2 class="document-title">{{ __('Receipt') }}</h2>
 
+        {{-- Logo and company info --}}
         <table width="100%">
             <tr>
-                <td valign="top">
+                {{-- Logo --}}
+                <td valign="top" align="center" width="40%">
                     <img src="{{ public_path('assets/img/no_image.jpg') }}" alt="Logo" class="logo" />
+                    <p class="company-name">MUNICIPALIDAD DE SANTA MARÍA CHIQUIMULA,
+                        TOTONICAPÁN</p>
                 </td>
-                <td class="company-info">
-                    <h3>Shinra Electric power company</h3>
-                    <p>
-                        asd
-                    </p>
+                {{-- Company info --}}
+                <td class="company-table">
+                    <table align="right">
+                        <tr class="company-info">
+                            <td>{{ __('Receipt No.') }}</td>
+                            <td class="rounded-square no">0001</td>
+                        </tr>
+                        <tr class="company-info">
+                            <td>{{ __('Amount') }}:</td>
+                            <td class="rounded-square">Q 105</td>
+                        </tr>
+                    </table>
                 </td>
             </tr>
         </table>
 
         <br>
 
-        {{-- Customer data --}}
+        {{-- Client info --}}
+        <div class="rounded" style="margin-top: -25px;">
+            <table width="100%" style="padding: 20px, 20px;">
+                <tr style="margin-bottom: 20px;">
+                    <td width="22%"><strong>{{ __('To Sr.') }}:</strong></td>
+                    <td class="border-bottom" width="45%">Pablo juan de la vega</td>
+                    <td align="right" class="border-bottom" width="15%">{{ __('Amount of') }}:</td>
+                    <td style="padding-left: 10px" class="border-bottom" width="20%">Q 500</td>
+                </tr>
+                <tr>
+                    <td><strong>{{ __('Amount in letters') }}:</strong></td>
+                    <td colspan="3" class="border-bottom">QUINIENTOS</td>
+                </tr>
+                <tr>
+                    <td><strong>{{ __('Concept of') }}:</strong></td>
+                    <td colspan="3" class="border-bottom">CAPA</td>
+                </tr>
+            </table>
+        </div>
+
+        {{-- Date and day of the week --}}
+        <table width="100%">
+            <tr align="right">
+                <td width="20%"><strong>{{ __('Date') }}:</strong></td>
+                <td align="left" style="padding-left: 10px">{{ date('d/m/Y', strtotime(date('d/m/Y'))) }}</td>
+                <td><strong>{{ __(date('l', strtotime(date('Y-m-d')))) }}
+                        {{ date('d', strtotime(date('Y-m-d'))) }} {{ __('of') }}
+                        {{ __(date('F', strtotime(date('Y-m-d')))) }} {{ __('of') }}
+                        {{ date('Y', strtotime(date('Y-m-d'))) }}</strong></td>
+            </tr>
+        </table>
+
+        <br>
+        <br>
+
+        {{-- Signature --}}
         <table width="100%">
             <tr>
-                <td><strong>From:</strong> Linblum - Barrio teatral</td>
-                <td><strong>To:</strong> Linblum - Barrio Comercial</td>
+                <td width="20%"></td>
+                <td class="signature">SOMOS CAPOS</td>
+                <td width="20%"></td>
             </tr>
         </table>
-
-        <br>
-
-        <table width="100%">
-            <thead class="table-title">
-                <tr>
-                    <th>#</th>
-                    <th>Description</th>
-                    <th>Quantity</th>
-                    <th>Unit Price $</th>
-                    <th>Total $</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Playstation IV - Black</td>
-                    <td align="right">1</td>
-                    <td align="right">1400.00</td>
-                    <td align="right">1400.00</td>
-                </tr>
-                <tr class="gray">
-                    <th scope="row">1</th>
-                    <td>Metal Gear Solid - Phantom</td>
-                    <td align="right">1</td>
-                    <td align="right">105.00</td>
-                    <td align="right">105.00</td>
-                </tr>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Final Fantasy XV - Game</td>
-                    <td align="right">1</td>
-                    <td align="right">130.00</td>
-                    <td align="right">130.00</td>
-                </tr>
-            </tbody>
-
-            <tfoot>
-                <tr>
-                    <td colspan="3"></td>
-                    <td align="right">Subtotal $</td>
-                    <td align="right">1635.00</td>
-                </tr>
-                <tr>
-                    <td colspan="3"></td>
-                    <td align="right">Tax $</td>
-                    <td align="right">294.3</td>
-                </tr>
-                <tr>
-                    <td colspan="3"></td>
-                    <td align="right">Total $</td>
-                    <td align="right" class="gray">$ 1929.3</td>
-                </tr>
-            </tfoot>
-        </table>
-
     </div>
 </body>
 
